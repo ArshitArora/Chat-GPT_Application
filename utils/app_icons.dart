6a662e8f37ceeb4a09f08f0cb8ed1bf80e3731fb
Arshit Arora <arshitkumar@gmail.com>
2023-05-16 11:50:08 +0530

    ok

---
+++ utils/app_icons.dart
@@ -0,0 +1,21 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppIcons {
+  /// Navigation Bar Icons
+
+  static const gptIcon = 'assets/chatgpt-icon.png';
+
+
+
+
+}
+
+class AppColor{
+  static const backGroundColor = Color(0xffff212a3e);
+  static var keyboardColor = Colors.black;
+  static const keyboardIconColor =  Color(0xffacacbe);
+  static const userPromptColor =  Color(0xffff394867);
+  static const aIPromptColor = Color(0xffff394867);
+}
